987c41103239816d0164b7f22bb360635a85adc7
Subodha Prabash <73874882+thelinuxuser-choice@users.noreply.github.com>
2021-03-26 15:05:44 +0530

    Update portex.py

--- portex.py
+++ portex.py
@@ -3,71 +3,8 @@
 #############subodha prabash ##########
 ###########port scanner ######
 
-# importing the necessary packages
-import time
-import sys
-import os
-
-
-# Function for implementing the loading animation
-def load_animation():
-    # String to be displayed when the application is loading
-    load_str = "loading Portex..."
-    ls_len = len(load_str)
-
-    # String for creating the rotating line
-    animation = "|/-\\"
-    anicount = 0
-
-    # used to keep the track of
-    # the duration of animation
-    counttime = 0
-
-    # pointer for travelling the loading string
-    i = 0
-
-    while (counttime != 30):
-
-        # used to change the animation speed
-        # smaller the value, faster will be the animation
-        time.sleep(0.075)
-
-        # converting the string to list
-        # as string is immutable
-        load_str_list = list(load_str)
-
-        # x->obtaining the ASCII code
-        x = ord(load_str_list[i])
-
-        # y->for storing altered ASCII code
-        y = 0
-
-        # if the character is "." or " ", keep it unaltered
-        # switch uppercase to lowercase and vice-versa
-        if x != 32 and x != 46:
-            if x > 90:
-                y = x - 32
-            else:
-                y = x + 32
-            load_str_list[i] = chr(y)
-
-            # for storing the resultant string
-        res = ''
-        for j in range(ls_len):
-            res = res + load_str_list[j]
-
-            # displaying the resultant string
-        sys.stdout.write("\r" + res + animation[anicount])
-        sys.stdout.flush()
-
-        # Assigning loading string
-        # to the resultant string
-        load_str = res
-
-        anicount = (anicount + 1) % 4
-        i = (i + 1) % ls_len
-        counttime = counttime + 1
-
+#clear
+def clear_sc():
     # for windows OS
     if os.name == "nt":
         os.system("cls")
@@ -76,11 +13,7 @@ def load_animation():
     else:
         os.system("clear")
 
-    # Driver program
-
-
-if __name__ == '__main__':
-    load_animation()
+clear_sc()
 #####end loding######
 
 # colored text and background
